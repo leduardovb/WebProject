ed65f13c99658d45601bc4c929350436d6eadd85
Eduardo <leduardovieirab@gmail.com>
2021-10-10 21:40:12 -0300

    create app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/home/Home'
+import Login from './components/login/Login'
+
 export default function App() {
   return (
-    <div>
-      <h1>Projeto Web</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
