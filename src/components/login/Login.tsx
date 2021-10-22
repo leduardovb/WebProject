a96a36f2eac3ddd6070c36f0e517a96d679a0ca5
Joaovsfc <j.vitorino2008@gmail.com>
2021-10-22 20:13:18 -0300

    João 22/10/2021 v1

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,5 +1,51 @@
-export default function Login() {
+import * as React from 'react';
+import Input from '@mui/material/Input';
+import Button from '@mui/material/Button';
+import './login.css';
+import { useState } from 'react';
+
+
+
+function Login() {
+
+    const [email,setEmail] = useState<string>("") 
+    const [senha,setSenha] = useState<string>("") 
+    const verifyLogin = () => {
+        console.log(email)
+        console.log(senha)
+        if(email === "teste@email.com" && senha === "123456"){
+            
+        }
+    }
+
+    
+
     return (
-        <h1>Login</h1>
+        <>
+        <div className="container">
+            <div className="box">
+                <form action="" className="form">
+                    <div className="labelCenter">
+                        <label>Email</label>
+                    </div>
+                    <div className="inputEmail">
+                        <Input onChange={ (e) => setEmail(e.target.value)} value={email}  placeholder="email@email" />
+                    </div>
+                    <div className="labelCenter">
+                        <label>Senha</label>
+                    </div>
+                    <div className="inputEmail">
+                        <Input onChange={ (e) => setSenha(e.target.value)} value={senha} placeholder="senha" />
+                    </div>  
+                    <br />
+                    <div className="labelCenter">
+                        <Button onClick={verifyLogin} variant="contained">Login</Button>
+                    </div>
+                </form>
+            </div>            
+        </div>
+        </>
     )
 }
+
+export default Login
