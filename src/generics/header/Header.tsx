d89fc1217d818429c75b58af559280fcab6600a1
Eduardo <leduardovieirab@gmail.com>
2021-10-25 19:18:45 -0300

    code style

--- src/generics/header/Header.tsx
+++ src/generics/header/Header.tsx
@@ -1,31 +1,36 @@
-import { Container, HeaderNavContainer, Logo, LogoContainer, MoneyCount, SubContainer } from "./HeaderBase"
-import WebLogo from "../../images/icons/logo.svg"
-import HeaderNav from "./components/headerNav/HeaderNav"
-import UserProfile from "./components/userProfile/UserProfile"
-import { Link } from "react-router-dom"
+import {
+	Container,
+	HeaderNavContainer,
+	Logo,
+	LogoContainer,
+	MoneyCount,
+	SubContainer,
+} from "./HeaderBase";
+import WebLogo from "../../images/icons/logo.svg";
+import HeaderNav from "./components/headerNav/HeaderNav";
+import UserProfile from "./components/userProfile/UserProfile";
+import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-        <Container>
-            <SubContainer>
-                <Link to="/" style={{textDecoration: "none"}} >
-                    <LogoContainer>
-                        <Logo src={WebLogo} />
-                        MyCase
-                    </LogoContainer>
-                </Link>
-                <HeaderNavContainer>
-                    <HeaderNav navTitle="Shop" />
-                </HeaderNavContainer>
-            </SubContainer>
-            <SubContainer>
-                <UserProfile />
-                <MoneyCount>
-                    R$ 100,00
-                </MoneyCount>
-            </SubContainer>
-        </Container>
-    )
+	return (
+		<Container>
+			<SubContainer>
+				<Link to="/" style={{ textDecoration: "none" }}>
+					<LogoContainer>
+						<Logo src={WebLogo} />
+						MyCase
+					</LogoContainer>
+				</Link>
+				<HeaderNavContainer>
+					<HeaderNav navTitle="Shop" />
+				</HeaderNavContainer>
+			</SubContainer>
+			<SubContainer>
+				<UserProfile />
+				<MoneyCount>R$ 100,00</MoneyCount>
+			</SubContainer>
+		</Container>
+	);
 }
 
-export default Header
+export default Header;
